f069a5de732452797796df9f6b041284f0b788cf
Usman Ali <usmanusman136@hotmail.com>
2019-10-03 14:25:58 +0500

    initial migrations  + the admin area customization

--- RealState/settings.py
+++ RealState/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'RealState.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realstatedb',
+        'USER':'postgres',
+        'PASSWORD': 'pakistan2546',
+        'HOST': 'localhost'
     }
 }
 
@@ -125,3 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'RealState/static')
 ]
+
+#Media folder settings 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
