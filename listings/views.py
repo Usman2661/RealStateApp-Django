c43fb8265b0ac1d43241fab55e5610dbe2610dad
Usman Ali <usmanusman136@hotmail.com>
2019-10-04 22:57:06 +0500

    showing the data on the listings page + pagination

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger,Paginator
+
+from .models import Listings
 
 # Create your views here.
 def index(request):
-    return render (request, 'listings/listings.html')
+    listings=Listings.objects.order_by('-list_date').filter(is_published=True)
 
+    paginator=Paginator(listings,3 )
+    page = request.GET.get('page')
+    paged_listings =paginator.get_page(page)
+
+    context={
+         'listings':paged_listings
+    }
+    return render (request, 'listings/listings.html',context)
 
 def search(request):
     return render (request, 'listings/search.html')
     
-def listing(request):
+def listing(request,listing_id):
     return render (request, 'listings/listing.html')
